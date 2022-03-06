b1033431dc3c3e386e6dbbb2ef8a406107041f46
AlternativeHistory <99097604+AlternativeHistory@users.noreply.github.com>
2022-03-06 00:16:33 -0600

    Add files via upload

---
+++ bbgarmy/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT bbgarmy
+#define COMPONENT_BEAUTIFIED bbgarmy
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_BBGARMY
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_BBGARMY
+#define DEBUG_SETTINGS DEBUG_SETTINGS_BBGARMY
+#endif
+#include "\x\\addons\main\script_macros.hpp"
---
+++ iruarmy/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT iruarmy
+#define COMPONENT_BEAUTIFIED iruarmy
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_IRUARMY
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_IRUARMY
+#define DEBUG_SETTINGS DEBUG_SETTINGS_IRUARMY
+#endif
+#include "\x\\addons\main\script_macros.hpp"
