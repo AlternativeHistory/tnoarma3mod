d17ff5350fc70384f81ddd365da235ce9b3c59b8
AlternativeHistory <99097604+AlternativeHistory@users.noreply.github.com>
2022-03-06 00:18:45 -0600

    Add files via upload

---
+++ oruarmy/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT oruarmy
+#define COMPONENT_BEAUTIFIED oruarmy
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_ORUARMY
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_ORUARMY
+#define DEBUG_SETTINGS DEBUG_SETTINGS_ORUARMY
+#endif
+#include "\x\\addons\main\script_macros.hpp"
