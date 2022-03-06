6824d3d20b10c60f9fa747d0ed622b984a505feb
AlternativeHistory <99097604+AlternativeHistory@users.noreply.github.com>
2022-03-06 00:19:10 -0600

    Add files via upload

---
+++ idbarmy/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT idbarmy
+#define COMPONENT_BEAUTIFIED idbarmy
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_IDBARMY
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_IDBARMY
+#define DEBUG_SETTINGS DEBUG_SETTINGS_IDBARMY
+#endif
+#include "\x\\addons\main\script_macros.hpp"
