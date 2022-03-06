a333a6cafcb7c2c26c6fba66e1454b04d3bade03
AlternativeHistory <99097604+AlternativeHistory@users.noreply.github.com>
2022-03-06 00:14:58 -0600

    Add files via upload

---
+++ insignia_addon/insig/Config.cpp
@@ -0,0 +1,68 @@
+class CfgPatches {
+
+	class insignia_addon
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		version = "1.0.0";
+		author[]= {"basicname"};
+		authorUrl = "http://insignia.alex109.de/";
+	};
+
+};
+
+	/*=================================================================
+	
+	http://alex109.de
+
+	-> my steam: http://steamcommunity.com/id/Alex109/
+	
+	-> If you need any help with this, go here: https://steamcommunity.com/groups/arma3insignias
+		and open a new discussion. I will then try to help you!
+	
+	=================================================================*/
+
+class CfgUnitInsignia
+{
+	
+	class insignia_test1
+	{
+		displayName = "Russian Black League";
+		author = "basicname";
+		texture = "\insig\icon\omsk.paa";
+		textureVehicle = "";
+	};
+	
+		class insignia_test2
+	{
+		displayName = "Republic of West Alaska";
+		author = "basicname";
+		texture = "\insig\icon\werbell.paa";
+		textureVehicle = "";
+	};
+	
+			class insignia_test3
+	{
+		displayName = "Ordenstaat Burgund";
+		author = "basicname";
+		texture = "\insig\icon\burgundypatch.paa";
+		textureVehicle = "";
+	};
+	
+				class insignia_test4
+	{
+		displayName = "Dirlewanger Brigade";
+		author = "basicname";
+		texture = "\insig\icon\dirlewanger";
+		textureVehicle = "";
+	};
+					class insignia_test5
+	{
+		displayName = "Holy Russian Empire";
+		author = "basicname";
+		texture = "\insig\icon\tabby";
+		textureVehicle = "";
+	};
+	
+};
